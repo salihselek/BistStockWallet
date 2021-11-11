f8a1f4ab02694f7d96dfc7d7362608da5e851b9f
SALIH <salihselek42@gmail.com>
2021-11-11 23:36:04 +0300

    stock fragment started

---
+++ src/main/java/com/example/biststockwallet/BistStockWalletApplication.kt
@@ -0,0 +1,8 @@
+package com.example.biststockwallet
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BistStockWalletApplication : Application() {
+}
---
+++ src/main/java/com/example/biststockwallet/ui/stocks/StockFragment.kt
@@ -0,0 +1,16 @@
+package com.example.biststockwallet.ui.stocks
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.biststockwallet.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class StockFragment : Fragment(R.layout.fragment_stocks) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
